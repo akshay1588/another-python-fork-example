6200bf026bb53f4491cc9ce8cab0a57c7cb6f415
Roman Rogowski <rrogowski@aurorasolar.com>
2022-03-29 09:26:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# another-python-fork-example
+
+Your assignment is as follows:
+
+1. Fork this repository.
+2. Run the tests in this repository using `pytest --headed`.
+3. One of the tests is failing. Your job is to find and fix the failing test to make it pass.
+    * **Note**: You should NOT change any lines containing `assert`.
+4. Once the failing test is passing, please submit a Pull Request!
+`
---
+++ tests/test_counter.py
@@ -0,0 +1,24 @@
+def test_counter_starts_at_one(page):
+	page.goto('https://counter.onlineclock.net/')
+
+	assert page.locator('#counter').text_content() == "1"
+
+def test_counter_counts_up_once_on_click(page):
+	page.goto('https://counter.onlineclock.net/')
+	page.locator('text="Count Up"').click()
+	assert page.locator('#counter').text_content() == "2"
+
+def test_counter_counts_up_twice_on_double_click(page):
+	page.goto('https://counter.onlineclock.net/')
+	page.locator('text="Count Up"').dblclick()
+	assert page.locator('#counter').text_content() == "3"
+
+def test_counter_counts_down_once_on_click(page):
+	page.goto('https://counter.onlineclock.net/')
+	page.locator('text="Count Up"').click()
+	assert page.locator('#counter').text_content() == "0"
+
+def test_counter_counts_down_twice_on_double_click(page):
+	page.goto('https://counter.onlineclock.net/')
+	page.locator('text="Count Down"').dblclick()
+	assert page.locator('#counter').text_content() == "-1"
